84b69ac7f0b1e49745196cb8772cfbee76deffa4
Alex Rodriguez <28873858+mengano-net@users.noreply.github.com>
2021-12-20 04:49:04 +0000

    no need to call vim.opt.termguicolors = true as it's alreacy called on  lua/options/ini.lua
    Signed-off-by: Alex Rodriguez <28873858+mengano-net@users.noreply.github.com>

--- lua/bufferline-config/init.lua
+++ lua/bufferline-config/init.lua
@@ -4,8 +4,7 @@ if not status_ok then
   return
 end
 
-vim.opt.termguicolors = true
 require("bufferline").setup{}
---
+
 -- mappings
 vim.api.nvim_set_keymap('n', '<Tab>', ':BufferLineCycleNext<CR>', {silent = true})
