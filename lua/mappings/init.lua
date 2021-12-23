4be325c06b26be79fe127291627acb8b91644532
Alex Rodriguez <28873858+mengano-net@users.noreply.github.com>
2021-12-23 22:15:00 +0000

    new tressitter config
    Signed-off-by: Alex Rodriguez <28873858+mengano-net@users.noreply.github.com>

--- lua/mappings/init.lua
+++ lua/mappings/init.lua
@@ -1,9 +1,12 @@
 local map = vim.api.nvim_set_keymap
 
-map('n', '<Leader>w', ':write<CR>', {noremap = true})
+-- https://github.com/nvim-treesitter/nvim-treesitter
+-- Under the troublwshooting section, it covers an issue similar to what I'm having, titled "I experience weird highlighting issues similar to #78"
+-- because of it, I'm comandeering the save map to also force-enable treesitter and reload buffer
+map('n', '<Leader>w', ':write | TSEnableAll highlight | edit<cr>', {noremap = true})
 map('i', 'jk', '<Esc>', {noremap = true})
 -- skip if module(s) isn't loaded
-local status_ok, comment = pcall(require, "bufferline")
+local status_ok, bufferline = pcall(require, "bufferline")
 if status_ok then
   map('n', '<Tab>', ':BufferLineCycleNext<CR>', {silent = true})
 end
--- lua/treesitter-config/init.lua
+++ lua/treesitter-config/init.lua
@@ -7,16 +7,12 @@ end
 treesitter.setup {
   -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   ensure_installed = {"lua", "yaml", "bash", "python" },
-  sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-  ignore_install = {}, -- List of parsers to ignore installing
+  sync_install = false,
+  ignore_install = {},
   highlight = {
-    enable = true,              -- false will disable the whole extension
-    -- disable = { "c", "rust" },  -- list of language that will be disabled
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
+    enable = true,
+    additional_vim_regex_highlighting = true,
   },
-  indent = { enable = true, disable = {} },
+  incremental_selection = { enable = true },
+  indent = { enable = true },
 }
