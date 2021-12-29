fbcf1db6777884b2965d0e05adf861325542dfae
Alex Rodriguez <28873858+mengano-net@users.noreply.github.com>
2021-12-29 06:11:06 +0000

    formatting
    Signed-off-by: Alex Rodriguez <28873858+mengano-net@users.noreply.github.com>

--- lua/treesitter-config/init.lua
+++ lua/treesitter-config/init.lua
@@ -10,21 +10,21 @@ treesitter.setup {
   ensure_installed = {"lua", "yaml", "bash", "python" },
   sync_install = false,
   ignore_install = {},
-  autopairs = {
-		enable = true,
-	},
-  rainbow = {
-    enable = true,
-    -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-    max_file_lines = nil, -- Do not enable for files with more than n lines, int
-    -- colors = {}, -- table of hex strings
-    -- termcolors = {} -- table of colour name strings
-  },
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = true,
   },
   incremental_selection = { enable = true },
   indent = { enable = true },
+  autopairs = {
+    enable = true,
+  },
+  rainbow = {
+    enable = true,
+    -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+    extended_mode = false, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+    max_file_lines = nil, -- Do not enable for files with more than n lines, int
+    -- colors = {}, -- table of hex strings
+    -- termcolors = {} -- table of colour name strings
+  },
 }
