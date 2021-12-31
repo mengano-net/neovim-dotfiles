d619caa17221a85ad8c283941b54179733de2947
Alex Rodriguez <28873858+mengano-net@users.noreply.github.com>
2021-12-31 03:46:52 +0000

    added some functions to open terminal using whichkey-config, but I had issues with sumneko_lua LSP server crashing
    Signed-off-by: Alex Rodriguez <28873858+mengano-net@users.noreply.github.com>

--- lua/whichkey-config/init.lua
+++ lua/whichkey-config/init.lua
@@ -4,10 +4,26 @@ if not status_ok then
   return
 end
 
+local opts = {prefix = '<leader>'}
+local Terminal = require("toggleterm.terminal").Terminal
+local toggle_float = function ()
+  local float = Terminal:new({direction = "float"})
+  return float:toggle()
+end
+local toggle_horizontal = function ()
+  local float = Terminal:new({direction = "horizontal"})
+  return float:toggle()
+end
+
+
 local mappings = {
   x = {":bd!<cr>", "Close buffer"},
   w = {":w<cr>", "Save buffer"},
+  -- t = {
+  --   name = "Terminal",
+  --   f = {toggle_float, "Floating Terminal"},
+  --   h = {toggle_float, "Horizontal Terminal"},
+  -- },
 }
-local opts = {prefix = '<leader>'}
 
 wk.register(mappings, opts)
