4e13b408d606830904a39831ad353d314c6b9507
Alex Rodriguez <28873858+mengano-net@users.noreply.github.com>
2021-12-31 05:02:15 +0000

    added module protectiont to exist is module is not loaded
    Signed-off-by: Alex Rodriguez <28873858+mengano-net@users.noreply.github.com>

--- lua/format-config/init.lua
+++ lua/format-config/init.lua
@@ -1,11 +1,11 @@
 -- Exit if can't load module(s)
--- local status_ok, format = pcall(require, "format")
--- if not status_ok then
---   return
--- end
+local status_ok, format = pcall(require, "format")
+if not status_ok then
+  return
+end
 
--- format.setup {
-require("format").setup {
+format.setup {
+-- require("format").setup {
   html = {{cmd = {"prettier -w"}}},
   css = {{cmd = {"prettier -w"}}},
   json = {{cmd = {"prettier -w"}}},
