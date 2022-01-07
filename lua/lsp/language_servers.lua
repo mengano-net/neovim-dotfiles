0411fd14549d84f44b7fa929f4e1d6900d5f1bdd
Alex Rodriguez <28873858+mengano-net@users.noreply.github.com>
2022-01-07 03:08:52 +0000

    wip - got null-ls going - fixed vim.lsp.buf.formatting() function on the on_attach function in   lua/lsp/language_servers - added a new vimscript function to call lua function above: augroup Format - installed rust and stylua binary - used newly implemented vim.lsp.buf.formatting() to format the   lua/plugins/init.lua file
    Signed-off-by: Alex Rodriguez <28873858+mengano-net@users.noreply.github.com>

--- lua/lsp/language_servers.lua
+++ lua/lsp/language_servers.lua
@@ -1,62 +1,58 @@
 -- Exit if can't load module(s)
 local status_ok, nvim_lsp = pcall(require, "lspconfig")
 if not status_ok then
-  return
+	return
 end
 
 vim.opt.completeopt = "menuone,noinsert,noselect"
 local buffer_map = vim.api.nvim_buf_set_keymap
 local buffer_option = vim.api.nvim_buf_set_option
-local map_opts = { noremap=true, silent=true }
+local map_opts = { noremap = true, silent = true }
 
-local custom_lsp_attach = function(client,bufnr)
+local custom_lsp_attach = function(client, bufnr)
+	-- Use LSP as the handler for omnifunc.
+	--    See `:help omnifunc` and `:help ins-completion` for more information.
+	buffer_option(0, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-  -- Use LSP as the handler for omnifunc.
-  --    See `:help omnifunc` and `:help ins-completion` for more information.
-  buffer_option(0, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+	-- Use LSP as the handler for formatexpr.
+	--    See `:help formatexpr` for more information.
+	buffer_option(0, "formatexpr", "v:lua.vim.lsp.formatexpr()")
 
-  -- Use LSP as the handler for formatexpr.
-  --    See `:help formatexpr` for more information.
-  buffer_option(0, 'formatexpr', 'v:lua.vim.lsp.formatexpr()')
+	-- Handlers
+	vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
+	vim.lsp.handlers["textDocument/signature_help"] = vim.lsp.with(
+		vim.lsp.handlers.signature_help,
+		{ border = "rounded" }
+	)
 
-  -- Handlers
-  vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {border = "rounded"})
-  vim.lsp.handlers["textDocument/signature_help"] = vim.lsp.with(vim.lsp.handlers.signature_help, {border = "rounded"})
-
-  -- buffer_map(bufnr, 'n', 'k', '<cmd>lua vim.lsp.buf.signature_help()<cr>', map_opts)
-  --[[ moved to whichkey
+	-- buffer_map(bufnr, 'n', 'k', '<cmd>lua vim.lsp.buf.signature_help()<cr>', map_opts)
+	--[[ moved to whichkey
   buffer_map(bufnr, 'n', 'gj', '<cmd>lua vim.lsp.diagnostic.goto_next()<cr>', map_opts)
   buffer_map(bufnr, 'n', 'gk', '<cmd>lua vim.lsp.diagnostic.goto_prev()<cr>', map_opts) ]]
-  buffer_map(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', map_opts)
-  buffer_map(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', map_opts)
-  buffer_map(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', map_opts)
+	buffer_map(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", map_opts)
+	buffer_map(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", map_opts)
+	buffer_map(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", map_opts)
 
-  if client.resolved_capabilities.textDocument_declaration then
-    buffer_map(bufnr, 'n', 'gD', '<cmd> lua vim.lsp.buf.declaration()<CR>', map_opts)
-  end
+	if client.resolved_capabilities.textDocument_declaration then
+		buffer_map(bufnr, "n", "gD", "<cmd> lua vim.lsp.buf.declaration()<CR>", map_opts)
+	end
 
-  if client.resolved_capabilities.document_formatting then
-    buffer_map(bufnr, 'n', 'gq', '<cmd>lua vim.lsp.buf.formatting()<CR>', map_opts)
-  elseif client.resolved_capabilities.document_range_formatting then
-    buffer_map(bufnr, 'n', 'gq', '<cmd>lua vim.lsp.buf.formatting()<CR>', map_opts)
-  end
-
-  -- highlight words under cursor
-  if client.resolved_capabilities.document_highlight then
-    vim.api.nvim_exec(
-      [[
+	-- highlight words under cursor
+	if client.resolved_capabilities.document_highlight then
+		vim.api.nvim_exec(
+			[[
       augroup lsp_document_highlight
         autocmd! * <buffer>
         autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
         autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
       augroup END
     ]],
-      false
-    )
-  end
+			false
+		)
+	end
 
-  --Debug code
-  --[[
+	--Debug code
+	--[[
   if client.resolved_capabilities.code_action then
     print("codeAction present")
   else
@@ -64,122 +60,123 @@ local custom_lsp_attach = function(client,bufnr)
   end
   ]]
 
-
-  -- Format on save BufWritePre to run lua formatting, conditional on server capabilities
-  if client.resolved_capabilities.document_formatting then
-    vim.api.nvim_command [[augroup Format]]
-    vim.api.nvim_command [[autocmd! * <buffer>]]
-    vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
-    vim.api.nvim_command [[augroup END]]
-  end
-  vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
+	vim.api.nvim_exec(
+		[[
+      augroup Format
+        autocmd! * <buffer>
+        autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()
+      augroup END
+      command! Format execute 'lua vim.lsp.buf.formatting()'
+    ]],
+		false
+	)
 end
 
 -- Setup capabilities
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 
-nvim_lsp.pylsp.setup {
-  cmd = { "pylsp" },
-  filetypes = { "python" },
-  on_attach = custom_lsp_attach,
-  capabilities = capabilities,
-}
+nvim_lsp.pylsp.setup({
+	cmd = { "pylsp" },
+	filetypes = { "python" },
+	on_attach = custom_lsp_attach,
+	capabilities = capabilities,
+})
 
 -- bash language server
-nvim_lsp.bashls.setup{
-  cmd = {"/usr/bin/bash-language-server", "start"},
-  on_attach = custom_lsp_attach,
-  capabilities = capabilities,
-  flags = {
-    debounce_text_changes = 150,
-  },
-  filetypes = { 'sh', 'zsh' },
-}
+nvim_lsp.bashls.setup({
+	cmd = { "/usr/bin/bash-language-server", "start" },
+	on_attach = custom_lsp_attach,
+	capabilities = capabilities,
+	flags = {
+		debounce_text_changes = 150,
+	},
+	filetypes = { "sh", "zsh" },
+})
 
 --yaml language server
-local yamlls_binary = ''
-if vim.fn.has('mac') == 1 then
-  yamlls_binary = '/usr/local/bin/yaml-language-server'
+local yamlls_binary = ""
+if vim.fn.has("mac") == 1 then
+	yamlls_binary = "/usr/local/bin/yaml-language-server"
 elseif vim.fn.has("unix") == 1 then
-  yamlls_binary = '/usr/bin/yaml-language-server'
+	yamlls_binary = "/usr/bin/yaml-language-server"
 end
 
-nvim_lsp.yamlls.setup{
-  cmd = {yamlls_binary, "--stdio"},
-  on_attach = custom_lsp_attach,
-  capabilities = capabilities,
-  debounce_text_changes = 150,
-  filetypes = { 'yaml', 'yml' },
-  settings = {
-    yaml = {
-      hover = true,
-      format = {
-        enable = true,
-        singleQuote = true,
-      },
-      completion = true,
-      validate = true,
-      customTags = {
-        '!Ref',
-        '!ImportValue',
-        '!Sub',
-        '!GetAtt',
-        '!GetAZs',
-        '!Join sequence',
-        '!Equals sequence',
-        '!FindInMap sequence',
-        '!Select sequence',
-        '!Split sequence',
-      },
-    },
-  },
-}
+nvim_lsp.yamlls.setup({
+	cmd = { yamlls_binary, "--stdio" },
+	on_attach = custom_lsp_attach,
+	capabilities = capabilities,
+	debounce_text_changes = 150,
+	filetypes = { "yaml", "yml" },
+	settings = {
+		yaml = {
+			hover = true,
+			format = {
+				enable = true,
+				singleQuote = true,
+			},
+			completion = true,
+			validate = true,
+			customTags = {
+				"!Ref",
+				"!ImportValue",
+				"!Sub",
+				"!GetAtt",
+				"!GetAZs",
+				"!Join sequence",
+				"!Equals sequence",
+				"!FindInMap sequence",
+				"!Select sequence",
+				"!Split sequence",
+			},
+		},
+	},
+})
 
 -- lua-language-server
 -- https://github.com/sumneko/lua-language-server/wiki/Build-and-Run-(Standalone)
-USER = vim.fn.expand('$USER')
+USER = vim.fn.expand("$USER")
 
 local sumneko_root_path = ""
 local sumneko_binary = ""
 
 if vim.fn.has("mac") == 1 then
-  sumneko_root_path = "/Users/" .. USER .. "/.local/lua-language-server"
-  sumneko_binary = "/Users/" .. USER .. "/.local/lua-language-server/bin/macOS/lua-language-server"
+	sumneko_root_path = "/Users/" .. USER .. "/.local/lua-language-server"
+	sumneko_binary = "/Users/" .. USER .. "/.local/lua-language-server/bin/macOS/lua-language-server"
 elseif vim.fn.has("unix") == 1 then
-  sumneko_root_path = "/home/" .. USER .. "/.local/lua-language-server"
-  sumneko_binary = "/home/" .. USER .. "/.local/lua-language-server/bin/lua-language-server"
+	sumneko_root_path = "/home/" .. USER .. "/.local/lua-language-server"
+	sumneko_binary = "/home/" .. USER .. "/.local/lua-language-server/bin/lua-language-server"
 else
-  print("Unsupported system for sumneko")
+	print("Unsupported system for sumneko")
 end
 
-nvim_lsp.sumneko_lua.setup {
-  on_attach = custom_lsp_attach,
-  cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"},
-  -- cmd = {"/home/ec2-user/github-repos/lua-language-server/bin/Linux/lua-language-server", "-E", "/home/ec2-user/github-repos/lua-language-server/" .. "main.lua"},
-  flags = {debounce_text_changes = 150},
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
-        -- Setup your lua path
-        path = vim.split(package.path, ';')
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = { 'vim' }
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = {
-          [vim.fn.expand('$VIMRUNTIME/lua')] = true,
-          [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
-          [vim.fn.expand('~/.config.nvim/lua')] = true,
-        }
-      }
-    }
-  }
-}
+nvim_lsp.sumneko_lua.setup({
+	on_attach = custom_lsp_attach,
+	cmd = { sumneko_binary, "-E", sumneko_root_path .. "/main.lua" },
+	-- cmd = {"/home/ec2-user/github-repos/lua-language-server/bin/Linux/lua-language-server", "-E", "/home/ec2-user/github-repos/lua-language-server/" .. "main.lua"},
+	flags = { debounce_text_changes = 150 },
+	settings = {
+		Lua = {
+			runtime = {
+				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+				version = "LuaJIT",
+				-- Setup your lua path
+				path = vim.split(package.path, ";"),
+			},
+			diagnostics = {
+				-- Get the language server to recognize the `vim` global
+				globals = { "vim" },
+			},
+			workspace = {
+				-- Make the server aware of Neovim runtime files
+				library = {
+					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+					[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+					[vim.fn.expand("~/.config.nvim/lua")] = true,
+				},
+			},
+		},
+	},
+})
 
 -- vim.lsp.set_log_level('debug')
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,124 +1,132 @@
 -- Automatically install packer
-local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-  print("Installing packer; you need to close and reopen Neovim...")
+	PACKER_BOOTSTRAP = vim.fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
+	print("Installing packer; you need to close and reopen Neovim...")
 end
 
 -- Have packer use a popup window
-require("packer").init {
-  display = {
-    open_fn = function()
-      return require("packer.util").float { border = "rounded" }
-    end,
-  },
-}
+require("packer").init({
+	display = {
+		open_fn = function()
+			return require("packer.util").float({ border = "rounded" })
+		end,
+	},
+})
 
 -- return require('packer').startup(function()
 return require("packer").startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = {'kyazdani42/nvim-web-devicons', opt = true},
-    config = "require('lualine-config')"
-  }
-  use {'akinsho/bufferline.nvim',
-    requires = 'kyazdani42/nvim-web-devicons',
-    config = { "require('bufferline-config')" }
-  }
-  use { 'windwp/nvim-autopairs', config = { "require('autopairs-config')" } }
-  use {'p00f/nvim-ts-rainbow'}
-  use 'tpope/vim-fugitive'
-  use {
-    'lewis6991/gitsigns.nvim',
-    requires = { 'nvim-lua/plenary.nvim' },
-    config = { "require('gitsigns-config')" }
-    -- tag = 'release' -- To use the latest release
-  }
-  use { 'numToStr/Comment.nvim', config = "require('comment-config')" }
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = { 'nvim-lua/plenary.nvim' },
-    config = { "require('telescope-config')", "require('telescope-extensions')" }
-  }
-  use { 'rcarriga/nvim-notify', config = "require('notify-extensions')" }
-  use {  'norcalli/nvim-colorizer.lua', config = "require('colorizer-config')" }
-  use 'ntpeters/vim-better-whitespace'
-  use { 'lukas-reineke/indent-blankline.nvim', config = "require('blankline-config')" }
-  use { 'akinsho/toggleterm.nvim', config = "require('toggleterm-config')" }
-  use {
-    'kyazdani42/nvim-tree.lua',
-    requires = {
-      'kyazdani42/nvim-web-devicons', -- optional, for file icon
-    },
-    config = {
-      -- function() require'nvim-tree'.setup {} end,
-      "require('nvimtree-config')",
-    }
-  }
-  use {
-    "folke/which-key.nvim",
-    config = function()
-      -- require("which-key").setup {}
-      require("whichkey-config")
-    end
-  }
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "kyazdani42/nvim-web-devicons", opt = true },
+		config = "require('lualine-config')",
+	})
+	use({
+		"akinsho/bufferline.nvim",
+		requires = "kyazdani42/nvim-web-devicons",
+		config = { "require('bufferline-config')" },
+	})
+	use({ "windwp/nvim-autopairs", config = { "require('autopairs-config')" } })
+	use({ "p00f/nvim-ts-rainbow" })
+	use("tpope/vim-fugitive")
+	use({
+		"lewis6991/gitsigns.nvim",
+		requires = { "nvim-lua/plenary.nvim" },
+		config = { "require('gitsigns-config')" },
+		-- tag = 'release' -- To use the latest release
+	})
+	use({ "numToStr/Comment.nvim", config = "require('comment-config')" })
+	use({
+		"nvim-telescope/telescope.nvim",
+		requires = { "nvim-lua/plenary.nvim" },
+		config = { "require('telescope-config')", "require('telescope-extensions')" },
+	})
+	use({ "rcarriga/nvim-notify", config = "require('notify-extensions')" })
+	use({ "norcalli/nvim-colorizer.lua", config = "require('colorizer-config')" })
+	use("ntpeters/vim-better-whitespace")
+	use({ "lukas-reineke/indent-blankline.nvim", config = "require('blankline-config')" })
+	use({ "akinsho/toggleterm.nvim", config = "require('toggleterm-config')" })
+	use({
+		"kyazdani42/nvim-tree.lua",
+		requires = {
+			"kyazdani42/nvim-web-devicons", -- optional, for file icon
+		},
+		config = {
+			-- function() require'nvim-tree'.setup {} end,
+			"require('nvimtree-config')",
+		},
+	})
+	use({
+		"folke/which-key.nvim",
+		config = function()
+			-- require("which-key").setup {}
+			require("whichkey-config")
+		end,
+	})
 
-  -- Treesitter
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = ":TSUpdate",
-    config = "require('treesitter-config')"
-  }
+	-- Treesitter
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+		config = "require('treesitter-config')",
+	})
 
-  -- LSP
-  use 'neovim/nvim-lspconfig'
-  use { 'onsails/lspkind-nvim', config = "require('lspkind-config')" }
-  --[[ I'm trying to retire LSPSaga, most of its functions are implemented builtin
+	-- LSP
+	use("neovim/nvim-lspconfig")
+	use({ "onsails/lspkind-nvim", config = "require('lspkind-config')" })
+	--[[ I'm trying to retire LSPSaga, most of its functions are implemented builtin
   use { 'tami5/lspsaga.nvim', config = "require('lspsaga-config')" } ]]
-  use {'tami5/lspsaga.nvim'}
-  use { 'williamboman/nvim-lsp-installer', config = "require('nvim-lsp-installer')" }
-  use {"jose-elias-alvarez/null-ls.nvim", config = "require('null-ls-config')"}
+	use({ "tami5/lspsaga.nvim" })
+	use({ "williamboman/nvim-lsp-installer", config = "require('nvim-lsp-installer')" })
+	use({ "jose-elias-alvarez/null-ls.nvim", config = "require('null-ls-config')" })
 
-  -- cmp
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-cmdline'
-  use 'hrsh7th/cmp-nvim-lua'
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-vsnip'
-  use 'hrsh7th/vim-vsnip'
+	-- cmp
+	use("hrsh7th/cmp-nvim-lsp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
+	use("hrsh7th/cmp-cmdline")
+	use("hrsh7th/cmp-nvim-lua")
+	use("hrsh7th/nvim-cmp")
+	use("hrsh7th/cmp-vsnip")
+	use("hrsh7th/vim-vsnip")
 
-  -- colorschemes
-  use { 'rose-pine/neovim' }
-  use 'shaunsingh/nord.nvim'
-  use { 'navarasu/onedark.nvim', setup = "vim.g.onedark_style = 'dark'" }
-  use {
-    'folke/tokyonight.nvim',
-    setup = { "vim.g.tokyonight_style = 'night'", "tokyonight_lualine_bold = 'true'" },
-    -- config = "vim.cmd('colorscheme tokyonight')"
-  }
-  use { "adisen99/apprentice.nvim", requires = { "rktjmp/lush.nvim" } }
-  use {
-    "ellisonleao/gruvbox.nvim",
-    requires = { "rktjmp/lush.nvim" },
-    setup = { "vim.g.gruvbox_contrast_dark = 'hard'" },
-    -- config = "vim.cmd('colorscheme gruvbox')"
-  }
-  use {
-    "marko-cerovac/material.nvim",
-    -- setup = "vim.g.material_style = 'deep ocean'",
-    setup = "vim.g.material_style = 'darker'",
-    -- config = { "vim.cmd('colorscheme material')", "require('material-config')" }
-    config = { "require('material-config')" }
-  }
-  use {"lunarvim/darkplus.nvim", config = "vim.cmd('colorscheme darkplus')"}
+	-- colorschemes
+	use({ "rose-pine/neovim" })
+	use("shaunsingh/nord.nvim")
+	use({ "navarasu/onedark.nvim", setup = "vim.g.onedark_style = 'dark'" })
+	use({
+		"folke/tokyonight.nvim",
+		setup = { "vim.g.tokyonight_style = 'night'", "tokyonight_lualine_bold = 'true'" },
+		-- config = "vim.cmd('colorscheme tokyonight')"
+	})
+	use({ "adisen99/apprentice.nvim", requires = { "rktjmp/lush.nvim" } })
+	use({
+		"ellisonleao/gruvbox.nvim",
+		requires = { "rktjmp/lush.nvim" },
+		setup = { "vim.g.gruvbox_contrast_dark = 'hard'" },
+		-- config = "vim.cmd('colorscheme gruvbox')"
+	})
+	use({
+		"marko-cerovac/material.nvim",
+		-- setup = "vim.g.material_style = 'deep ocean'",
+		setup = "vim.g.material_style = 'darker'",
+		-- config = { "vim.cmd('colorscheme material')", "require('material-config')" }
+		config = { "require('material-config')" },
+	})
+	use({ "lunarvim/darkplus.nvim", config = "vim.cmd('colorscheme darkplus')" })
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if PACKER_BOOTSTRAP then
-    require('packer').sync()
-  end
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if PACKER_BOOTSTRAP then
+		require("packer").sync()
+	end
 end)
