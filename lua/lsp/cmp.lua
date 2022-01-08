d53644ce5834de0e64fc25f86dfbd79b687522d4
Alex Rodriguez <28873858+mengano-net@users.noreply.github.com>
2022-01-08 20:17:56 +0000

    formatting
    Signed-off-by: Alex Rodriguez <28873858+mengano-net@users.noreply.github.com>

--- lua/lsp/cmp.lua
+++ lua/lsp/cmp.lua
@@ -1,23 +1,23 @@
 -- Exit if can't load module(s)
 local status_ok, cmp = pcall(require, "cmp")
 if not status_ok then
-  return
+	return
 end
 local status_ok, lspkind = pcall(require, "lspkind")
 if not status_ok then
-  return
+	return
 end
 
-vim.g.completeopt="menu,menuone,preview,noselect,noinsert"
+vim.g.completeopt = "menu,menuone,preview,noselect,noinsert"
 
 -- Functions to implement autocomplete on the custom <Tab> and
 -- <S-Tab> mappings below
 local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 local feedkey = function(key, mode)
-  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
 end
 
 -- Setup nvim-cmp.
@@ -25,105 +25,104 @@ end
 -- lspkind.init()
 
 -- cmp setup
-cmp.setup{
-  snippet = {
-    expand = function(args)
-      -- For `vsnip` user.
-      vim.fn["vsnip#anonymous"](args.body)
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			-- For `vsnip` user.
+			vim.fn["vsnip#anonymous"](args.body)
 
-      -- For `luasnip` user.
-      -- require('luasnip').lsp_expand(args.body)
+			-- For `luasnip` user.
+			-- require('luasnip').lsp_expand(args.body)
 
-      -- For `ultisnips` user.
-      -- vim.fn["UltiSnips#Anon"](args.body)
-    end,
-  },
+			-- For `ultisnips` user.
+			-- vim.fn["UltiSnips#Anon"](args.body)
+		end,
+	},
 
-  mapping = {
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm {
-      select = true,
-      behavior = cmp.ConfirmBehavior.Insert,
-    },
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif vim.fn["vsnip#available"]() == 1 then
-        feedkey("<Plug>(vsnip-expand-or-jump)", "")
-      elseif has_words_before() then
-        cmp.complete()
-      else
-        fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
-      end
-    end, { "i", "s" }),
-    ["<S-Tab>"] = cmp.mapping(function()
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-        feedkey("<Plug>(vsnip-jump-prev)", "")
-      end
-    end, { "i", "s" }),
-  },
+	mapping = {
+		["<C-d>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.close(),
+		["<CR>"] = cmp.mapping.confirm({
+			select = true,
+			behavior = cmp.ConfirmBehavior.Insert,
+		}),
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif vim.fn["vsnip#available"]() == 1 then
+				feedkey("<Plug>(vsnip-expand-or-jump)", "")
+			elseif has_words_before() then
+				cmp.complete()
+			else
+				fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+			end
+		end, { "i", "s" }),
+		["<S-Tab>"] = cmp.mapping(function()
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+				feedkey("<Plug>(vsnip-jump-prev)", "")
+			end
+		end, { "i", "s" }),
+	},
 
-  -- the order of these sources dictates appearance on complete menu
-  sources = cmp.config.sources {
-    { name = 'buffer', keyword_length = 4 },        -- type min of 4 chars before autocomplete kicks in
-    { name = 'nvim_lsp' },
-    { name = 'nvim_lua' },
-    { name = 'treesitter' },
-    { name = 'path' },
-    { name = 'vsnip' }, -- For vsnip users.
-    -- { name = 'luasnip' }, -- For luasnip users.
-    -- { name = 'ultisnips' }, -- For ultisnips users.
-    -- { name = 'snippy' }, -- For snippy users.
-    { name = 'spell' },
-  },
+	-- the order of these sources dictates appearance on complete menu
+	sources = cmp.config.sources({
+		{ name = "buffer", keyword_length = 4 }, -- type min of 4 chars before autocomplete kicks in
+		{ name = "nvim_lsp" },
+		{ name = "nvim_lua" },
+		{ name = "treesitter" },
+		{ name = "path" },
+		{ name = "vsnip" }, -- For vsnip users.
+		-- { name = 'luasnip' }, -- For luasnip users.
+		-- { name = 'ultisnips' }, -- For ultisnips users.
+		-- { name = 'snippy' }, -- For snippy users.
+		{ name = "spell" },
+	}),
 
-  formatting = {
-    format = lspkind.cmp_format {
-      with_text = true,
-      maxwidth = 50,
-      menu = {
-        buffer = "[buffer]",
-        nvim_lsp = "[LSP]",
-        nvim_lua = "[lua_api]",
-        path = "[path]",
-        spell = "[spell]",
-      },
-    },
-  },
+	formatting = {
+		format = lspkind.cmp_format({
+			with_text = true,
+			maxwidth = 50,
+			menu = {
+				buffer = "[buffer]",
+				nvim_lsp = "[LSP]",
+				nvim_lua = "[lua_api]",
+				path = "[path]",
+				spell = "[spell]",
+			},
+		}),
+	},
 
-  confirm_opts = {
-    behavior = cmp.ConfirmBehavior.Replace,
-    select = false,
-  },
+	confirm_opts = {
+		behavior = cmp.ConfirmBehavior.Replace,
+		select = false,
+	},
 
-  documentation = {
-    border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-  },
+	documentation = {
+		border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+	},
 
-  experimental = {
-    native_menu = false,
-    ghost_text = false,
-  },
-
-}
+	experimental = {
+		native_menu = false,
+		ghost_text = false,
+	},
+})
 
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline('/', {
-  sources = {
-    { name = 'buffer' }
-  }
+cmp.setup.cmdline("/", {
+	sources = {
+		{ name = "buffer" },
+	},
 })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(':', {
-  sources = cmp.config.sources({
-    { name = 'path' }
-  }, {
-    { name = 'cmdline' }
-  })
+cmp.setup.cmdline(":", {
+	sources = cmp.config.sources({
+		{ name = "path" },
+	}, {
+		{ name = "cmdline" },
+	}),
 })
