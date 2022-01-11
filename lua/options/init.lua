cb6f0cd776cafa41017a08c5f3e409d9554d1c19
Alex Rodriguez <28873858+mengano-net@users.noreply.github.com>
2022-01-11 02:52:04 +0000

    - autoformat using stylua - added autocmd for python files: changing colorcolumn to 80
    Signed-off-by: Alex Rodriguez <28873858+mengano-net@users.noreply.github.com>

--- lua/options/init.lua
+++ lua/options/init.lua
@@ -1,83 +1,86 @@
 -- First clear any possible maps present on "<Space>"
-vim.api.nvim_set_keymap("","<Space>", "<Nop>", {noremap = true, silent = true})
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-vim.g.leader = ' '
+vim.api.nvim_set_keymap("", "<Space>", "<Nop>", { noremap = true, silent = true })
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.g.leader = " "
 
 local options = {
-  smartcase = true,
-  laststatus = 2,
-  hlsearch = true,
-  incsearch = true,
-  ignorecase = false,
-  termguicolors = true,
-  background = 'dark',
-  ruler = true,
-  writebackup = false,
-  swapfile = false,
-  pastetoggle = '<F2>',
-  backspace = 'indent,eol,start',
-  showcmd = false,
-  showmode = false,
-  wildoptions = 'pum',
-  pumheight = 20,
-  hidden = true,
-  splitright = true,
-  splitbelow = true,
-  wildmenu = true,
-  redrawtime = 250,
-  softtabstop = 2,
-  tabstop = 2,
-  shiftwidth = 2,
-  textwidth = 99,
-  updatetime = 150,
-  backup = false,
-  timeoutlen = 1000,
-  list = true,
-  wrap = false,
-  number = true,
-  relativenumber = true,
-  cul = true,
-  cuc = false,
-  colorcolumn = '100',
-  signcolumn = 'yes:2',
-  clipboard = "unnamedplus",
-  autoindent = true,
-  expandtab = true,
-  scrolloff = 5,
-  sidescrolloff = 5,
-  dictionary = '/usr/share/dict/words',
-  conceallevel = 0,
-  cmdheight = 2,
-  fileencoding = "utf-8",
+	smartcase = true,
+	laststatus = 2,
+	hlsearch = true,
+	incsearch = true,
+	ignorecase = false,
+	termguicolors = true,
+	background = "dark",
+	ruler = true,
+	writebackup = false,
+	swapfile = false,
+	pastetoggle = "<F2>",
+	backspace = "indent,eol,start",
+	showcmd = false,
+	showmode = false,
+	wildoptions = "pum",
+	pumheight = 20,
+	hidden = true,
+	splitright = true,
+	splitbelow = true,
+	wildmenu = true,
+	redrawtime = 250,
+	softtabstop = 2,
+	tabstop = 2,
+	shiftwidth = 2,
+	textwidth = 99,
+	updatetime = 150,
+	backup = false,
+	timeoutlen = 1000,
+	list = true,
+	wrap = false,
+	number = true,
+	relativenumber = true,
+	cul = true,
+	cuc = false,
+	colorcolumn = "100",
+	signcolumn = "yes:2",
+	clipboard = "unnamedplus",
+	autoindent = true,
+	expandtab = true,
+	scrolloff = 5,
+	sidescrolloff = 5,
+	dictionary = "/usr/share/dict/words",
+	conceallevel = 0,
+	cmdheight = 2,
+	fileencoding = "utf-8",
 }
 
 for key, value in pairs(options) do
-  vim.opt[key] = value
+	vim.opt[key] = value
 end
-vim.opt.shortmess:append "c"
-vim.opt.whichwrap:append "<,>,[,],h,l"
-vim.opt.iskeyword:append "-,_"
-
+vim.opt.shortmess:append("c")
+vim.opt.whichwrap:append("<,>,[,],h,l")
+vim.opt.iskeyword:append("-,_")
 
 local buffer_options = {
-  tabstop = 2,
-  shiftwidth = 2,
-  expandtab = true
+	tabstop = 2,
+	shiftwidth = 2,
+	expandtab = true,
 }
 
 for key, value in pairs(buffer_options) do
-  vim.bo[key] = value
+	vim.bo[key] = value
 end
 
-
-vim.cmd[[
+vim.cmd([[
 " syntax enable
 filetype plugin indent on
 
 augroup highlight_on_yank
   autocmd!
   au TextYankPost * silent! lua vim.highlight.on_yank {higroup="IncSearch", timeout=750}
+augroup END
+
+augroup python
+  au!
+  au FileType python setlocal colorcolumn=80
 augroup END
 
 " Unset paste mode aboue on InsertLeave action, that is leaving insert mode
@@ -126,4 +129,4 @@ autocmd InsertLeave * silent! set nopaste
 "   autocmd filetype netrw :set signcolumn="no"
 " augroup END
 
-]]
+]])
