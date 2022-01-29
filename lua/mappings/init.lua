fa101c8d1f8a9df11f6ab7c8a8df6857908d7831
Alex Rodriguez <28873858+mengano-net@users.noreply.github.com>
2022-01-29 01:14:54 +0000

    move find/replace maps to whichkey-config
    Signed-off-by: Alex Rodriguez <28873858+mengano-net@users.noreply.github.com>

--- lua/mappings/init.lua
+++ lua/mappings/init.lua
@@ -2,11 +2,11 @@ local map = vim.api.nvim_set_keymap
 
 -- moved to whichkey
 -- map('n', '<Leader>w', ':write<cr>', {noremap = true})
-map('i', 'jk', '<Esc>', {noremap = true})
+map("i", "jk", "<Esc>", { noremap = true })
 -- skip if module(s) isn't loaded
 local status_ok, bufferline = pcall(require, "bufferline")
 if status_ok then
-  map('n', '<Tab>', ':BufferLineCycleNext<CR>', {silent = true})
+	map("n", "<Tab>", ":BufferLineCycleNext<CR>", { silent = true })
 end
 
 -- NvimTreeToggle
@@ -18,51 +18,52 @@ end ]]
 
 -- deprecated since I'm using toggeterm pluging
 -- map('n', '<Leader>sh', ':split term://zsh<cr>', {noremap = true})
-map('t', '<Esc>', '<C-\\><C-n>', {noremap = true})
+map("t", "<Esc>", "<C-\\><C-n>", { noremap = true })
 
 -- moved to whichkey
 -- map('n', '<leader>x', ':bd!<cr>', {noremap = true})
 -- map('n', 'nh', ':nohl<cr>', {noremap = true})
-map('n', '<leader>p', '"0p', {noremap = true}) -- paste yanked, not deleted, not system copied
+map("n", "<leader>p", '"0p', { noremap = true }) -- paste yanked, not deleted, not system copied
 
 -- better navigating splits
-map('n', '<C-h>', '<C-w><C-h>', {noremap = true})
-map('n', '<C-j>', '<C-w><C-j>', {noremap = true})
-map('n', '<C-k>', '<C-w><C-k>', {noremap = true})
-map('n', '<C-l>', '<C-w><C-l>', {noremap = true})
+map("n", "<C-h>", "<C-w><C-h>", { noremap = true })
+map("n", "<C-j>", "<C-w><C-j>", { noremap = true })
+map("n", "<C-k>", "<C-w><C-k>", { noremap = true })
+map("n", "<C-l>", "<C-w><C-l>", { noremap = true })
 
 -- While on visual mode, this allows selections to remain selected for aditional actions without
 -- loosing the visual block selection
-map('v', '<', '<gv', {noremap = true})
-map('v', '>', '>gv', {noremap = true})
+map("v", "<", "<gv", { noremap = true })
+map("v", ">", ">gv", { noremap = true })
 
 -- "Y" will select remainder of the line, start at cursor,
 -- similar to "D" and "C" for deleting and replacing respectively
 -- map('n', 'Y', 'y$', {noremap = true}) -- moved to whichkey
 
 -- Moving visual blocks up/down
-map('v', 'J', ':m \'>+1<cr>gv=gv', {noremap = true})
-map('v', 'K', ':m \'<-2<cr>gv=gv', {noremap = true})
+map("v", "J", ":m '>+1<cr>gv=gv", { noremap = true })
+map("v", "K", ":m '<-2<cr>gv=gv", { noremap = true })
 -- utils.map('i', '<c-j>', '<esc>:m .+1<cr>')
 -- utils.map('i', '<c-k>', '<esc>:m .-2<cr>')
 
 -- Keeping it centered when hitting `n` or `N` after a search
-map('n', 'n', 'nzzzv', {noremap = true})
-map('n', 'N', 'Nzzzv', {noremap = true})
-map('n', 'J', 'mzJ`z', {noremap = true})
+map("n", "n", "nzzzv", { noremap = true })
+map("n", "N", "Nzzzv", { noremap = true })
+map("n", "J", "mzJ`z", { noremap = true })
 
 -- Undo breakpoints
-map('i', ',', ',<c-g>u', {noremap = true})
-map('i', '.', '.<c-g>u', {noremap = true})
-map('i', '!', '!<c-g>u', {noremap = true})
-map('i', '?', '?<c-g>u', {noremap = true})
+map("i", ",", ",<c-g>u", { noremap = true })
+map("i", ".", ".<c-g>u", { noremap = true })
+map("i", "!", "!<c-g>u", { noremap = true })
+map("i", "?", "?<c-g>u", { noremap = true })
 
--- Press * to search word under cursor, then hit map below for search/replace.
-map('n', '<leader>r', ':%s///g<left><left>', {noremap = true})
--- same but with confirmation for each change
-map('n', '<leader>rc', ':%s///gc<left><left><left>', {noremap = true})
+-- moved these to whichkey
+-- -- Press * to search word under cursor, then hit map below for search/replace.
+-- map('n', '<leader>r', ':%s///g<left><left>', {noremap = true})
+-- -- same but with confirmation for each change
+-- map('n', '<leader>rc', ':%s///gc<left><left><left>', {noremap = true})
 
 -- Search/replace on a visual block. You can do that by pressing *, then visual select
-map('x', '<leader>r', ':%s///g<left><left>', {noremap = true})
+map("x", "<leader>r", ":%s///g<left><left>", { noremap = true })
 -- same but with confirmation for each change
-map('x', '<leader>rc', ':%s///gc<left><left><left>', {noremap = true})
+map("x", "<leader>rc", ":%s///gc<left><left><left>", { noremap = true })
--- lua/whichkey-config/init.lua
+++ lua/whichkey-config/init.lua
@@ -1,112 +1,138 @@
--- Exit if can't load module(s)
+--  if can't load module(s)
 local status_ok, which_key = pcall(require, "which-key")
 if not status_ok then
-  return
+	return
 end
 
 local opts = {
-  prefix = "<leader>",
-  mode = "n", -- NORMAL mode
-  buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-  silent = true, -- use `silent` when creating keymaps
-  noremap = true, -- use `noremap` when creating keymaps
-  nowait = true, -- use `nowait` when creating keymaps
+	prefix = "<leader>",
+	mode = "n", -- NORMAL mode
+	buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
+	silent = true, -- use `silent` when creating keymaps
+	noremap = true, -- use `noremap` when creating keymaps
+	nowait = true, -- use `nowait` when creating keymaps
 }
 
 local setup = {
-  plugins = {
-    spelling = {
-      enabled = false, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
-    },
-  },
-  presets = {
-    operators = false, -- adds help for operators like d, y, ... and registers them for motion / text object completion
-  },
-  window = {
-    border = "rounded", -- none, single, double, shadow
-  },
-  layout = {
-    align = "center", -- align columns left, center or right
-  },
-  ignore_missing = true, -- enable this to hide mappings for which you didn't specify a label
+	plugins = {
+		spelling = {
+			enabled = false, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+		},
+	},
+	presets = {
+		operators = false, -- adds help for operators like d, y, ... and registers them for motion / text object completion
+	},
+	window = {
+		border = "rounded", -- none, single, double, shadow
+	},
+	layout = {
+		align = "center", -- align columns left, center or right
+	},
+	ignore_missing = true, -- enable this to hide mappings for which you didn't specify a label
 }
 
 -- functions to toggle terminal windows, one floating, one horizontal
 local Terminal = require("toggleterm.terminal").Terminal
-local toggle_float = function ()
-  local float = Terminal:new({direction = "float"})
-  return float:toggle()
+local toggle_float = function()
+	local float = Terminal:new({ direction = "float" })
+	return float:toggle()
 end
-local toggle_horizontal = function ()
-  local float = Terminal:new({direction = "horizontal"})
-  return float:toggle()
+local toggle_horizontal = function()
+	local float = Terminal:new({ direction = "horizontal" })
+	return float:toggle()
 end
 
 -- Clearing some builtin maps that I will steal for my own.
-vim.api.nvim_set_keymap("","s", "<Nop>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("","S", "<Nop>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("", "s", "<Nop>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("", "S", "<Nop>", { noremap = true, silent = true })
 
 local mappings = {
-  ["b"] = {
-    l = {"<cmd>lua require('telescope.builtin').buffers{previewer = false, layout_config = {width = 0.5}}<cr>", "List"},
-    r = {"<cmd>Gitsigns reset_buffer<cr>", "Reset Buffer"},
-    s = {"<cmd> Gitsigns stage_buffer<cr>", "Stage Buffer"},
-  },
-  ["e"] = {
-    name = "Edit",
-    h = {"<cmd>nohl<cr>", "Highlights"},
-    Y = {"y$", "Yank remainder of line"}
-    -- p = {"<cmd>\"0p<cr>", "Paste previously yanked"}, -- this is not working for some reason
-  },
-  ["f"] = {"<cmd>lua require('telescope-extensions').find_files()<cr>", "Find Files" },
-  ["g"] = {
-    name = "Git / Goto",
-    b = {"<cmd>lua require('telescope-extensions').git_branches()<cr>", "Branches"},
-    c = {"<cmd>Git commit -s<cr>", "Commit"},
-    h = {
-      name = "Hunks",
-      b = {"<cmd>lua require'gitsigns'.blame_line{full=true}<cr>", "Blame Line"},
-      j = {"<cmd>Gitsigns next_hunk<cr>", "Next Hunk"},
-      k = {"<cmd>Gitsigns prev_hunk<cr>", "Previous Hunk"},
-      p = {"<cmd>Gitsigns preview_hunk<cr>", "Preview Hunk"},
-      r = {"<cmd>Gitsigns reset_hunk<cr>", "Reset Hunk"},
-      s = {"<cmd> Gitsigns stage_hunk<cr>", "Stage Hunk"},
-      u = {"<cmd Gitsigns undo_stage_hunk<cr>", "UnStage Hunk"}
-    },
-    j = {"<cmd>lua vim.lsp.diagnostic.goto_next()<cr>", "Next Diagnostic"},
-    k = {"<cmd>lua vim.lsp.diagnostic.goto_prev()<cr>", "Previous Diagnostic"},
-    l = {"<cmd>lua require('telescope-extensions').git_commits()<cr>", "List commits"},
-    s = {"<cmd>Ge:<cr>", "Stage?"},
-    p = {"<cmd>Git push<cr>", "Push"},
-  },
-  ["q"] = { "<cmd>q!<CR>", "Quit" },
-  p = {
-    name = "Packer",
-    c = { "<cmd>PackerCompile<cr>", "Compile" },
-    i = { "<cmd>PackerInstall<cr>", "Install" },
-    s = { "<cmd>PackerSync<cr>", "Sync" },
-    S = { "<cmd>PackerStatus<cr>", "Status" },
-    u = { "<cmd>PackerUpdate<cr>", "Update" },
-  },
-  ["s"] = {
-    name = "Search",
-    ["g"] = {":lua require('telescope.builtin').live_grep(require('telescope.themes').get_ivy())<cr>", "Live Grep"},
-    ["t"] = {":lua require('telescope.builtin').help_tags(require('telescope.themes').get_ivy())<cr>", "Help Tags"},
-    ["j"] = {":lua require('telescope.builtin').jumplist(require('telescope.themes').get_ivy())<cr>", "Jump Points"},
-    ["c"] = {":lua require('telescope.builtin').commands(require('telescope.themes').get_ivy())<cr>", "Commands"},
-    ["G"] = {"<cmd>lua require('telescope-extensions').grep_within_grep()<cr>", "Grep within grep"},
-    ["m"] = {":lua require('telescope.builtin').man_pages(require('telescope.themes').get_ivy())<cr>", "Man Pages"},
-    ["r"] = {":lua require('telescope.builtin').oldfiles(require('telescope.themes').get_ivy())<cr>", "Recent Files"},
-    -- t = {function() print("bar") end, "Foobar"}
-  },
-  ["t"] = {
-    name = "Terminal",
-    f = {toggle_float, "Floating"},
-    h = {toggle_horizontal, "Horizontal"},
-  },
-  ["T"] = { "<cmd>NvimTreeToggle<cr>", "Nvim Tree" },
-  ["x"] = {":bd!<cr>", "Close buffer"},
-  ["w"] = {":w!<cr>", "Save buffer"},
+	["b"] = {
+		l = {
+			"<cmd>lua require('telescope.builtin').buffers{previewer = false, layout_config = {width = 0.5}}<cr>",
+			"List",
+		},
+		r = { "<cmd>Gitsigns reset_buffer<cr>", "Reset Buffer" },
+		s = { "<cmd> Gitsigns stage_buffer<cr>", "Stage Buffer" },
+	},
+	["e"] = {
+		name = "Edit",
+		h = { "<cmd>nohl<cr>", "Highlights" },
+		Y = { "y$", "Yank remainder of line" },
+		-- p = {"<cmd>\"0p<cr>", "Paste previously yanked"}, -- this is not working for some reason
+	},
+	["f"] = { "<cmd>lua require('telescope-extensions').find_files()<cr>", "Find Files" },
+	["F"] = {
+		name = "Find",
+		r = { ":%s///g<left><left>", "Replace Globally" },
+		c = { ":%s///gc<left><left><left>", "Replace / Confirm" },
+	},
+	["g"] = {
+		name = "Git / Goto",
+		b = { "<cmd>lua require('telescope-extensions').git_branches()<cr>", "Branches" },
+		c = { "<cmd>Git commit -s<cr>", "Commit" },
+		h = {
+			name = "Hunks",
+			b = { "<cmd>lua require'gitsigns'.blame_line{full=true}<cr>", "Blame Line" },
+			j = { "<cmd>Gitsigns next_hunk<cr>", "Next Hunk" },
+			k = { "<cmd>Gitsigns prev_hunk<cr>", "Previous Hunk" },
+			p = { "<cmd>Gitsigns preview_hunk<cr>", "Preview Hunk" },
+			r = { "<cmd>Gitsigns reset_hunk<cr>", "Reset Hunk" },
+			s = { "<cmd> Gitsigns stage_hunk<cr>", "Stage Hunk" },
+			u = { "<cmd Gitsigns undo_stage_hunk<cr>", "UnStage Hunk" },
+		},
+		j = { "<cmd>lua vim.lsp.diagnostic.goto_next()<cr>", "Next Diagnostic" },
+		k = { "<cmd>lua vim.lsp.diagnostic.goto_prev()<cr>", "Previous Diagnostic" },
+		l = { "<cmd>lua require('telescope-extensions').git_commits()<cr>", "List commits" },
+		s = { "<cmd>Ge:<cr>", "Stage?" },
+		p = { "<cmd>Git push<cr>", "Push" },
+	},
+	["q"] = { "<cmd>q!<CR>", "Quit" },
+	p = {
+		name = "Packer",
+		c = { "<cmd>PackerCompile<cr>", "Compile" },
+		i = { "<cmd>PackerInstall<cr>", "Install" },
+		s = { "<cmd>PackerSync<cr>", "Sync" },
+		S = { "<cmd>PackerStatus<cr>", "Status" },
+		u = { "<cmd>PackerUpdate<cr>", "Update" },
+	},
+	["s"] = {
+		name = "Search",
+		["g"] = {
+			":lua require('telescope.builtin').live_grep(require('telescope.themes').get_ivy())<cr>",
+			"Live Grep",
+		},
+		["t"] = {
+			":lua require('telescope.builtin').help_tags(require('telescope.themes').get_ivy())<cr>",
+			"Help Tags",
+		},
+		["j"] = {
+			":lua require('telescope.builtin').jumplist(require('telescope.themes').get_ivy())<cr>",
+			"Jump Points",
+		},
+		["c"] = {
+			":lua require('telescope.builtin').commands(require('telescope.themes').get_ivy())<cr>",
+			"Commands",
+		},
+		["G"] = { "<cmd>lua require('telescope-extensions').grep_within_grep()<cr>", "Grep within grep" },
+		["m"] = {
+			":lua require('telescope.builtin').man_pages(require('telescope.themes').get_ivy())<cr>",
+			"Man Pages",
+		},
+		["r"] = {
+			":lua require('telescope.builtin').oldfiles(require('telescope.themes').get_ivy())<cr>",
+			"Recent Files",
+		},
+		-- t = {function() print("bar") end, "Foobar"}
+	},
+	["t"] = {
+		name = "Terminal",
+		f = { toggle_float, "Floating" },
+		h = { toggle_horizontal, "Horizontal" },
+	},
+	["T"] = { "<cmd>NvimTreeToggle<cr>", "Nvim Tree" },
+	["x"] = { ":bd!<cr>", "Close buffer" },
+	["w"] = { ":w!<cr>", "Save buffer" },
 }
 
 which_key.setup(setup)
