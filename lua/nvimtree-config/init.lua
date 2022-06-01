6f9df82bb424ea465b139f155619f9b21905c9f6
Alex Rodriguez <arodriguez@bluebeam.com>
2022-06-01 14:10:15 -0500

    moves options that authors moved to .setup() function, see `bit.ly/3vIpEOJ` for details
    Signed-off-by: Alex Rodriguez <arodriguez@bluebeam.com>

--- lua/nvimtree-config/init.lua
+++ lua/nvimtree-config/init.lua
@@ -1,33 +1,17 @@
 -- skip if module(s) isn't loaded
 local status_ok, nvimtree = pcall(require, "nvim-tree")
 if not status_ok then
-	return
+  return
 end
 
-local global_options = {
-	nvim_tree_highlight_opened_files = 1,
-	nvim_tree_git_hl = 1,
-}
+-- most of these options have now been migrated to the setup function, see this for an example of
+-- such migrations: bit.ly/3vIpEOJ
+-- local global_options = {
+-- }
 
-for key, value in pairs(global_options) do
-	vim.g[key] = value
-end
-
-vim.g.nvim_tree_show_icons = {
-	git = 1,
-}
-
-vim.g.nvim_tree_icons = {
-	git = {
-		unstaged = "",
-		staged = "S",
-		unmerged = "",
-		renamed = "➜",
-		deleted = "",
-		untracked = "U",
-		ignored = "◌",
-	},
-}
+-- for key, value in pairs(global_options) do
+--   vim.g[key] = value
+-- end
 
 nvimtree.setup({
   actions = {
@@ -36,12 +20,38 @@ nvimtree.setup({
       resize_window = true,
     },
   },
-  diable_netrw,
-	filters = { dotfiles = true },
-	git = { ignore = true },
+  disable_netrw = true,
+  diagnostics = {
+    enable = true,
+    show_on_dirs = true,
+    icons = {
+      hint = " ",
+      info = " ",
+      warning = " ",
+      error = " ",
+    },
+  },
+  filters = { dotfiles = true },
+  git = { ignore = true },
   renderer = {
     indent_markers = {
       enable = false,
+    },
+    highlight_opened_files = 1,
+    highlight_git = true,
+    icons = {
+      padding = " ",
+      glyphs = {
+        git = {
+          unstaged = "",
+          staged = "S",
+          unmerged = "",
+          renamed = "➜",
+          deleted = "",
+          untracked = "U",
+          ignored = "◌",
+        },
+      },
     },
   },
 })
