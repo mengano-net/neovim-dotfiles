d5d379d4fb91c18bed8fe1b4dd2b5ed71ee98041
Alex Rodriguez <arodriguez@bluebeam.com>
2022-06-28 15:43:56 -0500

    - changed keymap tpo scrollup on preview for autocomplete items
    Signed-off-by: Alex Rodriguez <arodriguez@bluebeam.com>

--- lua/lsp/cmp.lua
+++ lua/lsp/cmp.lua
@@ -48,7 +48,7 @@ cmp.setup({
 
   mapping = cmp.mapping.preset.insert {
     ["<C-d>"] = cmp.mapping.scroll_docs(4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-u>"] = cmp.mapping.scroll_docs(-4),
     ["<C-Space>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.close(),
     ["<CR>"] = cmp.mapping.confirm({
