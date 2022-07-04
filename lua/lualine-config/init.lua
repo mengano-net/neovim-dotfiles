0242e03c1015358cd4c3e3edc73302c3f0aa3cd8
Alex Rodriguez <28873858+mengano-net@users.noreply.github.com>
2022-07-04 12:18:02 -0500

    - changed colorscheme for lualine
    Signed-off-by: Alex Rodriguez <28873858+mengano-net@users.noreply.github.com>

--- lua/lualine-config/init.lua
+++ lua/lualine-config/init.lua
@@ -16,47 +16,47 @@ end
 
 -- Vim's working directory
 local function current_working_dir()
-  local cwd = string.sub(vim.fn.getcwd(), 12)
+  local cwd = string.sub(vim.fn.getcwd(), 0)
   return "~" .. cwd
 end
 
 lualine.setup({
   options = {
-    -- theme = 'tokyonight',
-		theme = "material",
-		-- theme = 'gruvbox',
-    -- theme = "darkplus",
-		icons_enabled = true,
-		extensions = { "fugitive" },
-		section_separators = " ",
-		component_separators = " ",
-	},
-	sections = {
-		lualine_a = {
-			"mode",
-		},
-		lualine_b = {
-			"branch",
-			"diff",
-		},
-		lualine_c = {
-			{
-				"filename", -- displays file status (readonly status, modified status)
-				file_status = true, -- 0 = just filename, 1 = relative path, 2 = absolute path
-				path = 1, -- Shortens path to leave 40 space in the window
-			},
-		},
-		lualine_x = {
-			{
-				"diagnostics",
-				sources = { "nvim_diagnostic" },
-				symbols = { error = " ", warn = " ", info = " ", hint = " " },
-			},
-			-- 'fileformat',
-			"filetype",
-			-- { "filetype", icon_only = true },
-		},
-		-- lualine_y = {},
-		lualine_y = { { current_buffer_number }, { current_date } },
-	},
+    theme = 'tokyonight',
+    -- theme = "material",
+    -- theme = 'gruvbox',
+    icons_enabled = true,
+    extensions = { "fugitive" },
+    section_separators = " ",
+    component_separators = " ",
+  },
+  sections = {
+    lualine_a = {
+      "mode",
+    },
+    lualine_b = {
+      "branch",
+      "diff",
+    },
+    lualine_c = {
+      {
+        "filename", -- displays file status (readonly status, modified status)
+        -- current_working_dir,
+        file_status = true, -- 0 = just filename, 1 = relative path, 2 = absolute path
+        path = 1, -- Shortens path to leave 40 space in the window
+      },
+    },
+    lualine_x = {
+      {
+        "diagnostics",
+        sources = { "nvim_diagnostic" },
+        symbols = { error = " ", warn = " ", info = " ", hint = " " },
+      },
+      -- 'fileformat',
+      "filetype",
+      -- { "filetype", icon_only = true },
+    },
+    -- lualine_y = {},
+    lualine_y = { { current_buffer_number }, { current_date } },
+  },
 })
