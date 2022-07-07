cf64710fe66f9cd4ceaf089af209710c24a24b40
Alex Rodriguez <28873858+mengano-net@users.noreply.github.com>
2022-07-06 21:56:54 -0500

    - added highlights to cmp-config bordered windows
    Signed-off-by: Alex Rodriguez <28873858+mengano-net@users.noreply.github.com>

--- lua/cmp-config/init.lua
+++ lua/cmp-config/init.lua
@@ -146,8 +146,8 @@ cmp.setup({
     end,
   },
   window = {
-    completion = cmp.config.window.bordered(),
-    documentation = cmp.config.window.bordered(),
+    completion = cmp.config.window.bordered(winhighlight),
+    documentation = cmp.config.window.bordered(winhighlight),
   },
   mapping = cmp.mapping.preset.insert({
     ['<C-d>'] = cmp.mapping.scroll_docs(4),
