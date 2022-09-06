628d8f6824298d21617b6010a5dc0c77a9a7c018
Mengano.Net <28873858+mengano-net@users.noreply.github.com>
2022-09-06 14:53:16 -0500

    feat(python): added prettier and autopep8 formatters
    Signed-off-by: Mengano.Net <28873858+mengano-net@users.noreply.github.com>

--- lua/null-ls-config/init.lua
+++ lua/null-ls-config/init.lua
@@ -1,7 +1,7 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-	print("Couldn't require module 'null_ls'.")
-	return
+  print("Couldn't require module 'null_ls'.")
+  return
 end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -12,12 +12,15 @@ local diagnostics = null_ls.builtins.diagnostics
 local completion = null_ls.builtins.completion
 
 null_ls.setup({
-	debug = false,
-	sources = {
-		-- formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-		-- formatting.black.with({ extra_args = { "--fast" } }),
-		formatting.stylua,
-		diagnostics.flake8,
-		completion.spell.with({}),
-	},
+  debug = false,
+  sources = {
+    formatting.prettier.with({
+      extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+    }),
+    -- formatting.black.with({ extra_args = { "--fast" } }),
+    formatting.stylua,
+    formatting.autopep8,
+    diagnostics.flake8,
+    completion.spell.with({}),
+  },
 })
