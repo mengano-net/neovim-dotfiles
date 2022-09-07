bf0a694f2a0349b51b85cf710077ffb5af3949d9
Mengano.Net <28873858+mengano-net@users.noreply.github.com>
2022-09-07 13:30:16 -0500

    fix(null-ls): added code actions and formatting
    Signed-off-by: Mengano.Net <28873858+mengano-net@users.noreply.github.com>

--- lua/null-ls-config/init.lua
+++ lua/null-ls-config/init.lua
@@ -4,23 +4,23 @@ if not null_ls_status_ok then
   return
 end
 
--- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
-local formatting = null_ls.builtins.formatting
--- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-local diagnostics = null_ls.builtins.diagnostics
--- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/completion
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTIN_CONFIG.md
+local code_actions = null_ls.builtins.code_actions
 local completion = null_ls.builtins.completion
+local diagnostics = null_ls.builtins.diagnostics
+local formatting = null_ls.builtins.formatting
+local hover = null_ls.builtins.hover
 
 null_ls.setup({
   debug = false,
   sources = {
+    completion.spell,
+    diagnostics.flake8,
     formatting.prettier.with({
       extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
     }),
     -- formatting.black.with({ extra_args = { "--fast" } }),
     formatting.stylua,
     formatting.autopep8,
-    diagnostics.flake8,
-    completion.spell.with({}),
   },
 })
