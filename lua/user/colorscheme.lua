6b3528911e0deab6d9461d3c67e651bd0f01fc87
Mengano.net <28873858+mengano-net@users.noreply.github.com>
2022-09-26 21:31:26 -0500

    chore(colorscheme): I will try tokyonight-night
    Signed-off-by: Mengano.net <28873858+mengano-net@users.noreply.github.com>

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,9 +1,15 @@
-local colorscheme = "catppuccin"
--- local colorscheme = "neobones"
+-- local colorscheme = "catppuccin"
+-- local colorscheme = "darkplus"
+local colorscheme = "tokyonight"
 
 local status_ok, _ = pcall(require, colorscheme)
 if not status_ok then
   print("colorscheme not found: " .. colorscheme)
+  return
+end
+
+if colorscheme == "tokyonight" then
+  pcall(vim.cmd, "colorscheme " .. "tokyonight-night")
   return
 end
 
