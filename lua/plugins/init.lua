b90da6be0e14d0a5e659c176fe0e1ffe45545056
Mengano.Net <28873858+mengano-net@users.noreply.github.com>
2022-09-29 17:20:11 -0500

    fix(nvim-comment-frame): moved config to plugin.lua
    Signed-off-by: Mengano.Net <28873858+mengano-net@users.noreply.github.com>

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -223,7 +223,21 @@ return require("packer").startup(function(use)
   use {
     's1n7ax/nvim-comment-frame',
     requires = { 'nvim-treesitter' },
-    config = "require('nvim-comment-frame-config')"
+    config = function()
+      require('nvim-comment-frame').setup({
+        disable_default_keymap = true,
+        frame_width = 90,
+        line_wrap_len = 80,
+        languages = {
+          python = {
+            frame_width = 61,
+            line_wrap_len = 52,
+            start_str = '# --',
+            end_str = '-- #',
+          },
+        }
+      })
+    end
   }
 
   ----------------------------------------------------------------------------------------
