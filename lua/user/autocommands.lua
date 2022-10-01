1bf14e0e2b7f62452536cc8112cb30625f580e23
Mengano.net <28873858+mengano-net@users.noreply.github.com>
2022-10-01 11:03:04 -0500

    fix!(autocommands): now using vim.opt_local
    Before I had an issue where certain autocommands were setting up certain
    options that were normally set using vim.api.nvim_set_option ... Using
    this meant that those options were set for the window, including all
    buffers. So, I had to then create autocommand to restore some of those
    options when switching buffer to another file type. Using vim.opt_local i
    sway cleaner and it doesn't require that hack
    
    Signed-off-by: Mengano.net <28873858+mengano-net@users.noreply.github.com>

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -1,30 +1,15 @@
+------------------------------------------------------------------------------------------
+--                          Declaring autocommand groups first                          --
+------------------------------------------------------------------------------------------
 local _general = vim.api.nvim_create_augroup("general_settings", { clear = true })
 local _git = vim.api.nvim_create_augroup("git", { clear = true })
 local _python = vim.api.nvim_create_augroup("python", { clear = true })
 local _documentation = vim.api.nvim_create_augroup("documentation", { clear = true })
 local _yaml = vim.api.nvim_create_augroup("yaml", { clear = true })
 
-----------------------------------------------------------------------
---        To reset window options that had been set by other autocmd--
---        This is needed because win options are set for the        --
---                 whole window, not only buffers,                  --
---             thus they carry over to other buffers.               --
---         I'd be nice to have these options available for buffers  --
-----------------------------------------------------------------------
-
-local function reset_win_options()
-  vim.api.nvim_win_set_option(0, "colorcolumn", "100")
-  vim.api.nvim_win_set_option(0, "spell", false)
-  vim.api.nvim_win_set_option(0, "linebreak", false)
-  vim.api.nvim_win_set_option(0, "conceallevel", 0)
-end
-
-vim.api.nvim_create_autocmd("BufLeave", {
-  group = _general,
-  pattern = { "*" },
-  callback = reset_win_options,
-})
-
+------------------------------------------------------------------------------------------
+--                                 General autocommands                                 --
+------------------------------------------------------------------------------------------
 vim.api.nvim_create_autocmd("TextYankPost", {
   group = _general,
   command = "lua vim.highlight.on_yank { higroup='IncSearch', timeout=750}",
@@ -53,7 +38,10 @@ vim.api.nvim_create_autocmd("FileType", {
 ----------------------------------------------------------------------
 --                           Python files                           --
 ----------------------------------------------------------------------
-local function filetypes_python() vim.api.nvim_win_set_option(0, "colorcolumn", "80") end
+-- local function filetypes_python() vim.api.nvim_win_set_option(0, "colorcolumn", "80") end
+local function filetypes_python()
+  vim.opt_local.colorcolumn = "80"
+end
 
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
   group = _python,
@@ -65,10 +53,10 @@ vim.api.nvim_create_autocmd({ "BufEnter" }, {
 --                     Documentation file types                     --
 ----------------------------------------------------------------------
 local documentation_file_options = function()
-  vim.api.nvim_win_set_option(0, "conceallevel", 2)
-  vim.api.nvim_win_set_option(0, "spell", true)
+  vim.opt_local.conceallevel = 2
+  vim.opt_local.spell = true
   vim.bo.fo = "awtq"
-  vim.wo.linebreak = true
+  vim.opt_local.linebreak = true
 end
 
 vim.api.nvim_create_autocmd("BufEnter", {
@@ -92,6 +80,3 @@ vim.api.nvim_create_autocmd("BufEnter", {
   pattern = { "*.yaml", "*.yml" },
   callback = yaml_file_options,
 })
-
-vim.cmd([[
-]])
