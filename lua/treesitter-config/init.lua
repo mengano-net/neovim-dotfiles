4b630ba5c24bdc80b15392bdbda86cf61953e278
Mengano.net <28873858+mengano-net@users.noreply.github.com>
2022-10-01 10:04:48 -0500

    chore(treesitter-config): delete old coment line and such
    Signed-off-by: Mengano.net <28873858+mengano-net@users.noreply.github.com>

--- lua/treesitter-config/init.lua
+++ lua/treesitter-config/init.lua
@@ -4,10 +4,9 @@ if not status_ok then
   return
 end
 
--- require('nvim-treesitter.configs').setup {
 treesitter.setup {
-  -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   autopairs = { enable = true },
+  -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   ensure_installed = { "lua", "bash", "python" },
   ignore_install = {},
   highlight = {
@@ -19,11 +18,9 @@ treesitter.setup {
   playground = { enable = true },
   rainbow = {
     enable = true,
-    -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
     extended_mode = false, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
     max_file_lines = nil, -- Do not enable for files with more than n lines, int
-    -- colors = {}, -- table of hex strings
-    -- termcolors = {} -- table of colour name strings
+    -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
   },
 }
