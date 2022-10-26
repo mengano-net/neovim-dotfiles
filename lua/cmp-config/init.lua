03e61e26fd4453045691533e9a04e820964d35a9
Mengano.Net <28873858+mengano-net@users.noreply.github.com>
2022-10-26 13:59:45 -0700

    fix(cmp): cmp_nvim_lsp.update_capabilities is deprecated
    Signed-off-by: Mengano.Net <28873858+mengano-net@users.noreply.github.com>

--- lua/cmp-config/init.lua
+++ lua/cmp-config/init.lua
@@ -202,7 +202,7 @@ cmp.setup.cmdline(":", {
 })
 
 -- Setup lspconfig.
-local capabilities = cmp_nvim_lsp.update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = cmp_nvim_lsp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 -- lua-language-server
 -- https://github.com/sumneko/lua-language-server/wiki/Build-and-Run-(Standalone)
