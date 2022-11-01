0dfc17a6d50b5d2b833d261985bf3e5689f27ae9
Mengano.net <28873858+mengano-net@users.noreply.github.com>
2022-10-31 20:27:08 -0500

    chore(indent): I'm switching to 4 shiftwidth man!!
    Signed-off-by: Mengano.net <28873858+mengano-net@users.noreply.github.com>

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -7,48 +7,48 @@ local colorscheme = "catppuccin"
 
 local status_ok, _ = pcall(require, colorscheme)
 if not status_ok then
-  print("colorscheme not found: " .. colorscheme)
-  return
+    print("colorscheme not found: " .. colorscheme)
+    return
 end
 
 if colorscheme == "tokyonight" then
-  require("tokyonight").setup({
-    style = "night",
-    styles = {
-      comments = { italic = false },
-      keywords = { italic = false },
-      sidebars = { "terminal" },
-      lualine_bold = false,
-    },
-  })
+    require("tokyonight").setup({
+        style = "night",
+        styles = {
+            comments = { italic = false },
+            keywords = { italic = false },
+            sidebars = { "terminal" },
+            lualine_bold = false,
+        },
+    })
 end
 
 if colorscheme == "nord" then
-  vim.g.nord_contrast = 'false'
-  vim.g.nord_borders = 'true'
-  vim.g.nord_disable_background = 'true'
-  vim.g.nord_italic = 'false'
+    vim.g.nord_contrast = 'false'
+    vim.g.nord_borders = 'true'
+    vim.g.nord_disable_background = 'true'
+    vim.g.nord_italic = 'false'
 end
 
 if colorscheme == "rose-pine" then
-  require('rose-pine').setup({
-    dark_variant = 'moon',
-    border = 'highlight_hi',
-    disable_italics = true,
-    --[[ highlight_groups = {
+    require('rose-pine').setup({
+        dark_variant = 'moon',
+        border = 'highlight_hi',
+        disable_italics = true,
+        --[[ highlight_groups = {
       ColorColumn = { bg = 'rose' }
     }, ]]
-  })
+    })
 end
 
 if colorscheme == "catppuccin" then
-  vim.cmd "let g:catppuccin_flavour = 'macchiato'"
-  require("catppuccin").setup({
-    styles = {
-      comments = {},
-      conditionals = {},
-    }
-  })
+    vim.cmd "let g:catppuccin_flavour = 'macchiato'"
+    require("catppuccin").setup({
+        styles = {
+            comments = {},
+            conditionals = {},
+        }
+    })
 end
 
 pcall(vim.cmd, "colorscheme " .. colorscheme)
