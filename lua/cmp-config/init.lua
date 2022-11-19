c4cb6b051a3d77f2e000a6fd76020697ecbdc5b3
Mengano.net <28873858+mengano-net@users.noreply.github.com>
2022-11-19 09:42:39 -0600

    chore(cmp): separated cmp and lsp configuration
    Signed-off-by: Mengano.net <28873858+mengano-net@users.noreply.github.com>

--- lua/cmp-config/init.lua
+++ lua/cmp-config/init.lua
@@ -5,12 +5,6 @@ if not cmp_status_ok then
     return
 end
 
-local cmp_nvim_lsp_status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
-if not cmp_nvim_lsp_status_ok then
-    print("Couldn't require module 'cmp_nvim_lsp'.")
-    return
-end
-
 local status_ok, lspkind = pcall(require, "lspkind")
 if not status_ok then
     print("Couldn't require module 'lspkind'.")
@@ -47,9 +41,6 @@ lspkind.init({
     },
 })
 
-local buffer_map = vim.api.nvim_buf_set_keymap
-local buffer_option = vim.api.nvim_buf_set_option
-local map_opts = { noremap = true, silent = true }
 local macchiato = require("catppuccin.palettes.macchiato")
 local mocha = require("catppuccin.palettes.mocha")
 
@@ -61,41 +52,6 @@ local has_words_before = function()
 end
 local feedkey = function(key, mode)
     vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
-end
-
-local custom_lsp_attach = function(client, bufnr)
-    --    See `:help formatexpr` for more information.
-    buffer_option(0, "formatexpr", "v:lua.vim.lsp.formatexpr()")
-
-    -- Handlers
-    --Debug code
-    -- To print capabilities present on buffer, execute this:
-    -- :lua =vim.lsp.get_active_clients()[1].server_capabilities
-
-    vim.lsp.handlers["textDocument/hover"] =
-    vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
-    vim.lsp.handlers["textDocument/signature_help"] =
-    vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
-
-    buffer_map(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", map_opts)
-    buffer_map(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", map_opts)
-    buffer_map(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", map_opts)
-
-    if client.server_capabilities.textDocument_declaration then
-        buffer_map(bufnr, "n", "gD", "<cmd> lua vim.lsp.buf.declaration()<CR>", map_opts)
-    end
-
-    if client.server_capabilities.documentFormattingProvider then
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            group = vim.api.nvim_create_augroup("document highlight", { clear = false }),
-            pattern = { "*" },
-            command = "lua vim.lsp.buf.format() vim.diagnostic.enable()",
-        })
-    end
-
-    local illuminate_status_ok, illuminate = pcall(require, "illuminate")
-    if not illuminate_status_ok then return end
-    illuminate.on_attach(client)
 end
 
 ---@cast cmp -?
@@ -241,148 +197,4 @@ cmp.setup.cmdline(":", {
     }, {
         { name = "cmdline" },
     }),
-})
-
--- Setup lspconfig.
-local capabilities = cmp_nvim_lsp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
-
--- lua-language-server
--- https://github.com/sumneko/lua-language-server/wiki/Build-and-Run-(Standalone)
-
-local sumneko_root_path = ""
-local sumneko_binary = ""
-USER = vim.fn.expand("$USER")
-
-if vim.fn.has("mac") == 1 then
-    sumneko_root_path = "/Users/"
-        .. USER
-        .. "/.local/share/nvim/lsp_servers/sumneko_lua/extension/server"
-    sumneko_binary = "/Users/"
-        .. USER
-        .. "/.local/share/nvim/lsp_servers/sumneko_lua/extension/server/bin/lua-language-server"
-elseif vim.fn.has("unix") == 1 then
-    sumneko_root_path = "/home/" .. USER .. "/.local/lua-language-server"
-    sumneko_binary = "/home/" .. USER .. "/.local/lua-language-server/bin/lua-language-server"
-else
-    print("Can't find lua-language-server binary.")
-end
-
--- lua-language-server
-require("lspconfig")["sumneko_lua"].setup({
-    -- cmd = { sumneko_binary, "-E", sumneko_root_path .. "/main.lua" },
-    capabilities = capabilities,
-    on_attach = custom_lsp_attach,
-    flags = { debounce_text_changes = 150 },
-    settings = {
-        Lua = {
-            runtime = {
-                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                version = "LuaJIT",
-                -- Setup your lua path
-                path = vim.split(package.path, ";"),
-            },
-            diagnostics = {
-                globals = { "vim" }, -- Get the language server to recognze the `vim` global
-                -- This sets up lua-language-server to check code styles too, like indentation best practices, etc
-                --[[ neededFileStatus = {
-          ["codestyle-check"] = "Any",
-        }, ]]
-            },
-            workspace = {
-                -- Make the server aware of Neovim runtime files
-                library = vim.api.nvim_get_runtime_file("", true),
-            },
-            format = {
-                enable = true,
-                defaultConfig = {
-                    indent_style = "space",
-                    indent_size = "2",
-                },
-            },
-            telemetry = { enable = false }, -- Do not send randomized but unique telemetry data
-        },
-    },
-})
-
--- Python
---[[ local pylsp_root_binary = ""
-if vim.fn.has("mac") == 1 then
-  pylsp_root_binary = "/Users/" .. USER .. "/.local/share/nvim/lsp_servers/pylsp/venv/bin/pylsp"
-elseif vim.fn.has("unix") == 1 then
-else
-  print("Unsupported system for pylsp")
-end
-
-require("lspconfig")["pylsp"].setup({
-  -- cmd = { "pylsp" },
-  cmd = { pylsp_root_binary },
-  filetypes = { "python" },
-  on_attach = custom_lsp_attach,
-  capabilities = capabilities,
-}) ]]
-require("lspconfig")["pyright"].setup({
-    filetypes = { "python" },
-    on_attach = custom_lsp_attach,
-    capabilities = capabilities,
-})
-
--- bash language server
-require("lspconfig")["bashls"].setup({
-    -- cmd = { "/usr/bin/bash-language-server", "start" },
-    -- cmd == { "~/.local/share/nvim/lsp_servers/bash", "start" },
-    on_attach = custom_lsp_attach,
-    capabilities = capabilities,
-    flags = {
-        debounce_text_changes = 150,
-    },
-    filetypes = { "sh", "zsh" },
-})
-
---yaml language server
---[[ local yamlls_binary = ""
-if vim.fn.has("mac") == 1 then
-  yamlls_binary = "/usr/local/bin/yaml-language-server"
-elseif vim.fn.has("unix") == 1 then
-  yamlls_binary = "/usr/bin/yaml-language-server"
-end ]]
-
-require("lspconfig")["yamlls"].setup({
-    -- cmd = { yamlls_binary, "--stdio" },
-    on_attach = custom_lsp_attach,
-    capabilities = capabilities,
-    debounce_text_changes = 150,
-    filetypes = { "yaml", "yml" },
-    settings = {
-        yaml = {
-            hover = true,
-            format = {
-                enable = true,
-                singleQuote = true,
-            },
-            completion = true,
-            validate = true,
-            SchemeStore = { enable = true },
-            customTags = {
-                "!fn",
-                "!And",
-                "!If",
-                "!Not",
-                "!Equals",
-                "!Or",
-                "!Base64",
-                "!Cidr",
-                "!Ref",
-                "!ImportValue",
-                "!Sub sequence",
-                "!Sub scalar",
-                "!GetAtt",
-                "!GetAZs",
-                "!Join sequence",
-                "!Equals sequence",
-                "!FindInMap sequence",
-                "!Select sequence",
-                "!Split sequence",
-            },
-        },
-    },
 })
