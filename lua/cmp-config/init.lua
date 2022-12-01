ffc95093b0fe88c1f1f51e4ce0466500a765eefe
Mengano.net <28873858+mengano-net@users.noreply.github.com>
2022-11-30 20:37:05 -0800

    fix(cmp): was throwing exception when hit Tab with typing first
    Signed-off-by: Mengano.net <28873858+mengano-net@users.noreply.github.com>

--- lua/cmp-config/init.lua
+++ lua/cmp-config/init.lua
@@ -116,11 +116,11 @@ cmp.setup({
             c = cmp.mapping.close(),
         }),
         -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
         ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
-            elseif luasnip.expendable() then
+            elseif luasnip.expand_or_locally_jumpable() then
                 luasnip.expand()
             elseif luasnip.expand_or_jumpable() then
                 luasnip.expand_or_jump()
