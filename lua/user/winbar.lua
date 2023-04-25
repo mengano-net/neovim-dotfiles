a442d2b2c55f320d7e34fae0082a43d66990abff
Mengano <28873858+mengano-net@users.noreply.github.com>
2023-04-25 14:33:32 -0500

    Develop (#51)
    * doc(README): neovim needs to be version 0.9.x now
    
    Signed-off-by: Mengano.Net <28873858+mengano-net@users.noreply.github.com>
    
    * feat(winbar): implemented my own module.
    
    - I had a plugin(winbar)
    - I deleted it and implemented my own via autocmd on BufWinEnter
    - I also added new highlights for the colorscheme
    
    Signed-off-by: Mengano.net <28873858+mengano-net@users.noreply.github.com>
    
    * fix(winbar): fixing bugs
    
    Signed-off-by: Mengano.net <28873858+mengano-net@users.noreply.github.com>
    
    * feat(winbar): better colors and caps for winbar
    
    Signed-off-by: Mengano.net <28873858+mengano-net@users.noreply.github.com>
    
    * fix(winbar): issue with WinBar highlight
    
    Signed-off-by: Alex Rodriguez <alex@mengano.net>
    
    * chore(winbar): changed 'modified' icon on winbar
    
    Signed-off-by: Mengano.Net <28873858+mengano-net@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Mengano.Net <28873858+mengano-net@users.noreply.github.com>
    Signed-off-by: Mengano.net <28873858+mengano-net@users.noreply.github.com>
    Signed-off-by: Alex Rodriguez <alex@mengano.net>

--- lua/user/winbar.lua
+++ lua/user/winbar.lua
@@ -5,16 +5,13 @@ local M = {}
 
 function M.eval()
     local file_path = vim.api.nvim_eval_statusline("%f", {}).str
-    local modified = vim.api.nvim_eval_statusline("%M", {}).str == '+' and ' ⌾' or ''
+    local modified = vim.api.nvim_eval_statusline("%M", {}).str == '+' and ' ◉' or ''
 
     file_path = file_path:gsub('/', ' ➤ ')
 
     vim.api.nvim_set_hl(0, 'WinBarCaps', { fg = '#dedede', bg = '#363636' })
     vim.api.nvim_set_hl(0, 'WinBarPath', { fg = '#363636', bg = '#dedede' })
-    vim.api.nvim_set_hl(0, 'WinBarModified', {
-        bg = vim.api.nvim_get_hl(0, { name = "WinBar" }).bg,
-        fg = '#ff3838'
-    })
+    vim.api.nvim_set_hl(0, 'WinBarModified', { fg = '#ff3838' })
 
     return '%#WinBarCaps#'
         .. ''
