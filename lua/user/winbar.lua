bedba20d3079e38b6a5edd09d6224a0d64ab177a
Mengano.Net <28873858+mengano-net@users.noreply.github.com>
2023-04-25 14:05:52 -0500

    chore(winbar): changed 'modified' icon on winbar
    Signed-off-by: Mengano.Net <28873858+mengano-net@users.noreply.github.com>

--- lua/user/winbar.lua
+++ lua/user/winbar.lua
@@ -5,7 +5,7 @@ local M = {}
 
 function M.eval()
     local file_path = vim.api.nvim_eval_statusline("%f", {}).str
-    local modified = vim.api.nvim_eval_statusline("%M", {}).str == '+' and ' ⌾' or ''
+    local modified = vim.api.nvim_eval_statusline("%M", {}).str == '+' and ' ◉' or ''
 
     file_path = file_path:gsub('/', ' ➤ ')
 
