b3373af8ec916b3d2886d4e616574b1c61f58ed9
Mengano.Net <28873858+mengano-net@users.noreply.github.com>
2023-05-31 16:00:34 -0500

    fix(null-ls-config): prettier now obeys nvim's tabsize
    Signed-off-by: Mengano.Net <28873858+mengano-net@users.noreply.github.com>

--- lua/null-ls-config/init.lua
+++ lua/null-ls-config/init.lua
@@ -1,7 +1,7 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-  print("Couldn't require module 'null_ls'.")
-  return
+    print("Couldn't require module 'null_ls'.")
+    return
 end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTIN_CONFIG.md
@@ -12,21 +12,29 @@ local formatting = null_ls.builtins.formatting
 local hover = null_ls.builtins.hover
 
 null_ls.setup({
-  debug = false,
-  sources = {
-    code_actions.gitsigns,
-    completion.spell,
-    diagnostics.flake8.with({
-      extra_args = { "--extend-ignore=E501" }
-    }),
-    formatting.prettier.with({
-      extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
-      filetypes = { "html", "json", "yaml", "markdown" },
-    }),
-    -- formatting.black.with({ extra_args = { "--fast" } }),
-    formatting.stylua.with({
-      extra_args = { "--config-path", vim.fn.expand("~/.config/nvim/stylua.toml") },
-    }),
-    formatting.autopep8,
-  },
+    debug = false,
+    sources = {
+        code_actions.gitsigns,
+        completion.spell,
+        diagnostics.flake8.with({
+            extra_args = { "--extend-ignore=E501" }
+        }),
+        formatting.prettier.with({
+            -- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTIN_CONFIG.md
+            extra_args = function(params)
+                return params.options
+                    and params.options.tabSize
+                    and {
+                        "--tab-width",
+                        params.options.tabSize,
+                    }
+            end,
+            filetypes = { "html", "json", "yaml", "markdown" },
+        }),
+        -- formatting.black.with({ extra_args = { "--fast" } }),
+        formatting.stylua.with({
+            extra_args = { "--config-path", vim.fn.expand("~/.config/nvim/stylua.toml") },
+        }),
+        formatting.autopep8,
+    },
 })
