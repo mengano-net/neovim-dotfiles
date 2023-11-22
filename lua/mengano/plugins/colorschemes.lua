42ae0ae463f35d2fb677605a1e26bf7724675a1f
Mengano.Net <28873858+mengano-net@users.noreply.github.com>
2023-11-22 12:13:45 -0600

    chore: removed orphan files from previous config
    Signed-off-by: Mengano.Net <28873858+mengano-net@users.noreply.github.com>

--- lua/mengano/plugins/colorschemes.lua
+++ lua/mengano/plugins/colorschemes.lua
@@ -54,4 +54,15 @@ return {
         end,
     },
     { "folke/tokyonight.nvim" },
+    config = function()
+        require("tokyonight").setup({
+            style = "moon",
+            styles = {
+                comments = { italic = false },
+                keywords = { italic = false },
+                sidebars = { "terminal" },
+                lualine_bold = false,
+            },
+        })
+    end,
 }
