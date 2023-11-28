5f4a5d194ed818ea4f375b10aa4eeba94174a797
Mengano.Net <28873858+mengano-net@users.noreply.github.com>
2023-11-28 14:34:35 -0800

    fix: increased timeout for formatters to 1 second
    Signed-off-by: Mengano.Net <28873858+mengano-net@users.noreply.github.com>

--- lua/mengano/plugins/lsp/formatting.lua
+++ lua/mengano/plugins/lsp/formatting.lua
@@ -17,7 +17,7 @@ return {
             format_on_save = {
                 lsp_fallback = true,
                 async = false,
-                timeout_ms = 500,
+                timeout_ms = 1000,
             },
         })
 
@@ -28,7 +28,7 @@ return {
                 conform.format({
                     lsp_fallback = true,
                     async = false,
-                    timeout_ms = 500,
+                    timeout_ms = 1000,
                 })
             end,
             { desc = "Format file or range (in visual mode)" }
