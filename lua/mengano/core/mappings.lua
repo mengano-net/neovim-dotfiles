3af3b7c75bccb0c32a42b05e99f91a6bc064f712
Alex Rodriguez <alex@mengano.net>
2023-12-03 13:45:35 -0600

    chore: better mappings
    Signed-off-by: Alex Rodriguez <alex@mengano.net>

--- lua/mengano/core/mappings.lua
+++ lua/mengano/core/mappings.lua
@@ -1,7 +1,11 @@
-local keymap = vim.keymap.set
-local opts = { silent = true }
+-- Function wrapper for mapping custom keybindings
+local function keymap(mode, lhs, rhs, opts)
+    local options = { noremap = true, silent = true }
+    if opts then options = vim.tbl_extend("force", options, opts) end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
 
-keymap("i", "jk", "<Esc>", opts)
+keymap("i", "jk", "<Esc>")
 
 -- When you remove a selection character under cursor with `x`, do not also copy
 -- to register
@@ -11,34 +15,37 @@ keymap("n", "<Tab>", ":bnext<CR>", { silent = true })
 keymap("n", "<S-Tab>", ":bprevious<CR>", { silent = true })
 
 -- paste yanked, not deleted, not system copied
-keymap("n", "<leader>p", '"0p', opts)
+keymap("n", "<leader>p", '"0p')
+
+-- Replace all instances of highlighted words
+keymap("v", "<leader>r", '"hy:%s/<C-r>h//g<left><left>')
 
 -- While on visual mode, this allows selections to remain selected for aditional
 -- actions without loosing the visual block selection
-keymap("v", "<", "<gv", opts)
-keymap("v", ">", ">gv", opts)
+keymap("v", "<", "<gv")
+keymap("v", ">", ">gv")
 
 -- Moving visual blocks up/down
-keymap("v", "J", ":m '>+1<cr>gv=gv", opts)
-keymap("v", "K", ":m '<-2<cr>gv=gv", opts)
+keymap("v", "J", ":m '>+1<cr>gv=gv")
+keymap("v", "K", ":m '<-2<cr>gv=gv")
 
 -- Splits
-keymap("n", "ss", ":new<Return><C-w>j", opts)
-keymap("n", "sv", ":vnew<Return><C-w>l", opts)
--- keymap("n", "<C-h>", "<C-w>h", opts)
--- keymap("n", "<C-j>", "<C-w>j", opts)
--- keymap("n", "<C-k>", "<C-w>k", opts)
--- keymap("n", "<C-l>", "<C-w>l", opts)
--- keymap("n", "<C-Up>", ":resize -2<CR>", opts)
--- keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+keymap("n", "ss", ":new<Return><C-w>j")
+keymap("n", "sv", ":vnew<Return><C-w>l")
+-- keymap("n", "<C-h>", "<C-w>h")
+-- keymap("n", "<C-j>", "<C-w>j")
+-- keymap("n", "<C-k>", "<C-w>k")
+-- keymap("n", "<C-l>", "<C-w>l")
+-- keymap("n", "<C-Up>", ":resize -2<CR>")
+-- keymap("n", "<C-Down>", ":resize +2<CR>")
 
 -- Keeping it centered when hitting `n` or `N` after a search
-keymap("n", "n", "nzzzv", opts)
-keymap("n", "N", "Nzzzv", opts)
-keymap("n", "J", "mzJ`z", opts)
+keymap("n", "n", "nzzzv")
+keymap("n", "N", "Nzzzv")
+keymap("n", "J", "mzJ`z")
 
 -- Undo breakpoints
-keymap("i", ",", ",<c-g>u", opts)
-keymap("i", ".", ".<c-g>u", opts)
-keymap("i", "!", "!<c-g>u", opts)
-keymap("i", "?", "?<c-g>u", opts)
+keymap("i", ",", ",<c-g>u")
+keymap("i", ".", ".<c-g>u")
+keymap("i", "!", "!<c-g>u")
+keymap("i", "?", "?<c-g>u")
