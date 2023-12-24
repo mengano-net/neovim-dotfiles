e0ee9206072d8ab680fa4e064d6f4fcbcbdbba0a
Mengano <28873858+mengano-net@users.noreply.github.com>
2023-12-24 14:10:57 -0600

    chore: replaced lsp on_attach funtion with autocmd on LspAttach event (#74)
    * feat: added actions-preview plugin
    
    This plugin displays code actions from LSP into a telescope window
    
    Signed-off-by: Alex Rodriguez <alex@mengano.net>
    
    * fix: added filetype exceptions for cmp automcomplete
    
    for now they are TelescopePrompt and gitcommit
    
    Signed-off-by: Alex Rodriguez <alex@mengano.net>
    
    * chore: removed lsp on_attach function
    
    - I felt this function wasn't needed anymore. A while back there was a
      new autocmd event introduced into neovim ... 'LspAttach' so I'm using it to
      declare some LSP options instead of a custom on_attach function.
    
    Signed-off-by: Alex Rodriguez <alex@mengano.net>
    
    ---------
    
    Signed-off-by: Alex Rodriguez <alex@mengano.net>

--- lua/mengano/plugins/which-key.lua
+++ lua/mengano/plugins/which-key.lua
@@ -187,14 +187,10 @@ local mappings = {
         u = { "<cmd Gitsigns undo_stage_hunk<cr>", "UnStage Hunk" },
     },
     l = {
-        name = "LSP",
-        -- a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
+        name = "LSP",p
         a = { '<cmd>lua require("actions-preview").code_actions()<cr>', "Code Action" },
-        -- require("actions-preview").code_actions()
-        -- d = { "<cmd>lua vim.lsp.buf.definition()<cr>", "Definition" },
         d = { "<cmd>Glance definitions<cr>", "Definition" },
         h = { "<cmd>lua vim.lsp.buf.hover()<cr>", "Hover Definition" },
-        -- r = { "<cmd>lua vim.lsp.buf.references()<cr>", "References" },
         r = { "<cmd>Glance references<cr>", "References" },
         R = { "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename variable" },
         s = {
