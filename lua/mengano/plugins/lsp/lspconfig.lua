ccc43b06c93c230fc369a2f699219eafbcfedb14
Alex Rodriguez <alex@mengano.net>
2024-02-25 12:05:13 -0600

    chore: better nerd font icons for hint, error, etc
    Signed-off-by: Alex Rodriguez <alex@mengano.net>

--- lua/mengano/plugins/lsp/lspconfig.lua
+++ lua/mengano/plugins/lsp/lspconfig.lua
@@ -53,7 +53,7 @@ return {
                 },
                 severity_sort = true,
             })
-            local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+            local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
             for type, icon in pairs(signs) do
                 local hl = "DiagnosticSign" .. type
                 vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
