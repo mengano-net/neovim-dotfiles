131311a15ba90fd7390868e0e2c2ac6988ca8196
Alex Rodriguez <alex@mengano.net>
2024-04-08 15:16:37 -0500

    feat: added todo-comments plugin
    Signed-off-by: Alex Rodriguez <alex@mengano.net>

---
+++ lua/mengano/plugins/todo-comments.lua
@@ -0,0 +1,18 @@
+return {
+    "folke/todo-comments.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+        local todo_comments = require("todo-comments")
+
+        vim.keymap.set("n", "<leader>gt", function()
+            todo_comments.jump_next()
+        end, { desc = "Todo comment next" })
+
+        -- vim.keymap.set("n", "<leader>gtp", function()
+        --     todo_comments.jump_prev()
+        -- end, { desc = "Todo comment previous" })
+
+        todo_comments.setup()
+    end
+}
