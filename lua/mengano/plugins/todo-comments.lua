6446000a762f881de30eb5873038eb436ac7ecb3
Alex Rodriguez <alex@mengano.net>
2024-04-15 09:05:21 -0500

    feature: changed todo-comment to use telescope
    - todo-comments provides a built-in command "TodoTelescope" to bring up
      list of all todos in a telescope window.
    
    Signed-off-by: Alex Rodriguez <alex@mengano.net>

--- lua/mengano/plugins/todo-comments.lua
+++ lua/mengano/plugins/todo-comments.lua
@@ -5,16 +5,11 @@ return {
     config = function()
         local todo_comments = require("todo-comments")
 
-        -- TODO: find a way to instead do :TodoTelescope with get_ivy theme
         vim.keymap.set("n", "<leader>gt", function()
             -- todo_comments.jump_next()
-            vim.cmd("TodoLocList")
+            -- TODO: wrap in if statement to first check for telescope plugin
+            vim.cmd("TodoTelescope")
         end, { desc = "Todo comments" })
-
-        -- TODO: we need previous too, otherwise navigation seems cumbersom
-        -- vim.keymap.set("n", "<leader>gtp", function()
-        --     todo_comments.jump_prev()
-        -- end, { desc = "Todo comment previous" })
 
         todo_comments.setup()
     end,
