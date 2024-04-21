aceb0cc562c7b800fb40654e1c1cbc73cbeed648
Alex Rodriguez <alex@mengano.net>
2024-04-21 11:07:38 -0500

    chore: added markdown_lint to treesitter ensure_installed
    Signed-off-by: Alex Rodriguez <alex@mengano.net>

--- lua/mengano/plugins/treesitter.lua
+++ lua/mengano/plugins/treesitter.lua
@@ -5,22 +5,33 @@ return {
         dependencies = { "p00f/nvim-ts-rainbow" },
         opts = {
             autopairs = { enable = true },
-            ensure_installed = { "vimdoc", "lua", "bash", "markdown", "python", "toml", "yaml" },
+            ensure_installed = {
+                "vimdoc",
+                "lua",
+                "bash",
+                "markdown",
+                "markdown_inline",
+                "python",
+                "toml",
+                "yaml",
+            },
             auto_install = true,
             sync_install = false,
             ignore_install = {},
             highlight = {
                 enable = true,
-                additional_vim_regex_highlighting = false, -- See comments on github
+                additional_vim_regex_highlighting = false,
             },
             incremental_selection = { enable = true },
             indent = { enable = true },
             playground = { enable = true },
             rainbow = {
                 enable = true,
-                -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+                -- Also highlight non-bracket delimiters like html tags,
+                -- boolean or table: lang -> boolean
                 extended_mode = false,
-                max_file_lines = nil, -- Do not enable for files with more than n lines, int
+                -- Do not enable for files with more than n lines, int
+                max_file_lines = nil,
             },
         },
     },
